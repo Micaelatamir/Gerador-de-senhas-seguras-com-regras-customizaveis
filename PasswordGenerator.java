007c3e873f49b783279de6cd8ae21172b2d4e91b
Micaela Tamires <micaela.tamir@gmail.com>
2025-07-29 14:12:00 -0300

    Create java.security

---
+++ PasswordGenerator.java
@@ -0,0 +1,19 @@
+public import java.security.SecureRandom;
+
+public class PasswordGenerator {
+
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*()-_=+[]{};:,.<>?";
+
+    private SecureRandom random = new SecureRandom();
+
+    public String generatePassword(int length, boolean useUpper, boolean useLower, boolean useNumbers, boolean useSpecial) {
+        StringBuilder charPool = new StringBuilder();
+
+    
+}
+ 
+    
+
