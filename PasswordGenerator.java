9c463b08b6fe4084654481037ddfeaab49f79cf6
Micaela Tamires <micaela.tamir@gmail.com>
2025-07-29 14:13:35 -0300

    update

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -12,6 +12,31 @@ public class PasswordGenerator {
     public String generatePassword(int length, boolean useUpper, boolean useLower, boolean useNumbers, boolean useSpecial) {
         StringBuilder charPool = new StringBuilder();
 
+        if (useUpper) charPool.append(UPPERCASE);
+        if (useLower) charPool.append(LOWERCASE);
+        if (useNumbers) charPool.append(NUMBERS);
+        if (useSpecial) charPool.append(SPECIAL_CHARS);
+
+        if (charPool.length() == 0) {
+            throw new IllegalArgumentException("Pelo menos uma categoria de caracteres deve ser selecionada!");
+        }
+         StringBuilder password = new StringBuilder();
+
+        // Garante que pelo menos um de cada categoria selecionada entre na senha
+        if (useUpper) password.append(randomChar(UPPERCASE));
+        if (useLower) password.append(randomChar(LOWERCASE));
+        if (useNumbers) password.append(randomChar(NUMBERS));
+        if (useSpecial) password.append(randomChar(SPECIAL_CHARS));
+
+        // Preenche o restante da senha
+        for (int i = password.length(); i < length; i++) {
+            password.append(randomChar(charPool.toString()));
+        }
+
+        // Embaralha a senha para não ficar previsível
+        return shuffleString(password.toString());
+    }
+
     
 }
  
